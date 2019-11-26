be9a70d66daa703c4105a050aaa6d28bfa34c040
Klopfsalat <58117688+Klopfsalat@users.noreply.github.com>
2019-11-26 21:44:32 +0100

    Update Ausgabensammlung.java

--- 4Way4/src/fourWayFour/Ausgabensammlung.java
+++ 4Way4/src/fourWayFour/Ausgabensammlung.java
@@ -1,10 +1,14 @@
 package fourWayFour;
 
-
 public class Ausgabensammlung {
-	
-	
-	public static void ausgabe(String s){
+
+	protected static void ausgabeMitAbsatz(String s) {
 		System.out.println(s);
+	}
+	protected static void ausgabeMitAbsatz() {
+		System.out.println();
+	}
+	protected static void ausgabe(String s) {
+		System.out.print(s);
 	}
 }
