09e35219bc747e6a4ed054635b03912bb42ffa34
Klopfsalat <58117688+Klopfsalat@users.noreply.github.com>
2019-12-13 18:01:20 +0100

    Update Requirements.java

--- 4Way4/src/fourWayFour/Requirements.java
+++ 4Way4/src/fourWayFour/Requirements.java
@@ -2,10 +2,10 @@ package fourWayFour;
 
 public interface Requirements {
 
-	public void myMove(String eingabe);
+	public void myMove(String input);
 	public String yourMove();
 	public boolean isRunning();
 	public boolean whoWon();
 	public void printBoard();
-	public boolean isVaildMove(String eingabe);
+	public boolean isVaildMove(String input);
 }
