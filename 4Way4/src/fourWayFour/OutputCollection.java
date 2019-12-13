0abd0e84bc692a372ed243723cc1d4b5f09bfacf
Klopfsalat <58117688+Klopfsalat@users.noreply.github.com>
2019-12-13 21:23:47 +0100

    Update OutputCollection.java

--- 4Way4/src/fourWayFour/OutputCollection.java
+++ 4Way4/src/fourWayFour/OutputCollection.java
@@ -1,15 +1,31 @@
 package fourWayFour;
 
+/**
+ * Sammlung der Konsolen-Ausgaben
+ */
 public class OutputCollection {
 
+	/**
+	 * Ausgabe von String s mit Absatz
+	 * 
+	 * @param s
+	 */
 	protected static void outputWithNewLine(String s) {
 		System.out.println(s);
 	}
 
+	/**
+	 * Ausgabe von Absatz
+	 */
 	protected static void outputWithNewLine() {
 		System.out.println();
 	}
 
+	/**
+	 * Ausgabe von String s
+	 * 
+	 * @param s
+	 */
 	protected static void output(String s) {
 		System.out.print(s);
 	}
