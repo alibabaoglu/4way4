4312ca24d115fe4350fbc6da4c38a08b8915617a
Klopfsalat <58117688+Klopfsalat@users.noreply.github.com>
2019-12-13 17:30:03 +0100

    Update GameException.java

--- 4Way4/src/fourWayFour/GameException.java
+++ 4Way4/src/fourWayFour/GameException.java
@@ -1,7 +1,7 @@
 package fourWayFour;
 
 @SuppressWarnings("serial")
-public class GameException extends RuntimeException {
+public class GameException extends IllegalArgumentException {
 
 	public GameException(String msg) {
 		super(msg);
