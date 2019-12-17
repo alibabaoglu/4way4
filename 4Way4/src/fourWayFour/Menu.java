654f0403f77a8a2c5eb3d0f047006149473b6359
alibabaoglu <44380731+alibabaoglu@users.noreply.github.com>
2019-12-17 22:11:51 +0100

    Update Menu.java

--- 4Way4/src/fourWayFour/Menu.java
+++ 4Way4/src/fourWayFour/Menu.java
@@ -3,104 +3,94 @@ package fourWayFour;
 import java.util.*;
 
 public class Menu {
-
-	final static int SPIEL_BEENDEN = 0;
 	final static int COMPUTER = 1;
 	final static int SPIELER = 2;
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-
-		String spieler1 = null;
-		String spieler2 = null;
-		int gspieler;
-		int spielzug = 0;
-		int zeilenlaenge;
-		int spaltenlaenge;
-		int wiederholen;
-		boolean gueltigeEingabe;
-		GameBoard gb;
-		char spieler;
+		Scanner sc = new Scanner(System.in);
+		GameEngine engine;
+		boolean validInput = false, validMove = true;
+		String move;
+		int row, col, modus, beginner;
 
 		do {
+			System.out.println("Wählen Sie einen Spielmouds:\n \"1\" "
+					+ "fuer ein Spiel gegen KI\n \"2\"fuer ein Spiel gegen einen anderen Spieler");
 
-			// Auswahl Spielmodus
+			modus = sc.nextInt();
+			if (modus == COMPUTER || modus == SPIELER)
+				validInput = true;
+			else
+				System.out.println("Ungültige Eingabe:" + modus);
+		} while (!validInput);
+
+		do {
+			System.out.println("Geben Sie eine Spaltenlaenge fuer das Spielbrett zwischen 7 und 10 ein.");
+			col = sc.nextInt();
+			if (col < 7 || col > 10) {
+				System.out.println("Ungueltige Eingabe: " + col);
+				validInput = false;
+			} else
+				validInput = true;
+
+		} while (!validInput);
+
+		do {
+			System.out.println("Geben Sie eine Zeilenlaenge fuer das Spielbrett zwischen 7 und 10 ein.");
+			row = sc.nextInt();
+			if (row < 7 || row > 10) {
+				System.out.println("Ungueltige Eingabe:" + row);
+				validInput = false;
+			} else
+				validInput = true;
+
+		} while (!validInput);
+
+		if (modus == COMPUTER) {
+			System.out.println("Wenn KI anfangen soll, dann druecke die \"3\"");
+			if (sc.nextInt() == 3)
+				beginner = COMPUTER;
+			else
+				beginner = SPIELER;
+
+			engine = new GameEngine(row, col, COMPUTER, beginner);
+		} else
+			engine = new GameEngine(row, col, SPIELER);
+
+		while (engine.isRunning()) {
+
 			do {
-				OutputCollection.outputWithNewLine(("Druecken Sie die '1' fuer ein Spiel gegen den Computer" + "\n\t"
-						+ "oder die '2' fuer ein Spiel gegen einen anderen Spieler."));
-				gspieler = in.nextInt();
-				if (gspieler != COMPUTER && gspieler != SPIELER) {
-					OutputCollection.outputWithNewLine(("Ungueltige Eingabe!"));
-					gueltigeEingabe = false;
-				} else {
-					gueltigeEingabe = true;
-				}
-			} while (gueltigeEingabe == false);
+				if (!validMove)
+					System.out.println("Ungueltiger Zug");
 
-			// Auswahl Hoehe des Spielbretts
+				System.out.println("Spieler-1 ist am Zug:");
+				move = sc.next();
+				validMove = engine.isVaildMove(move);
+			} while (!validMove);
+
+			engine.myMove(move);
+			
+
 			do {
-				OutputCollection.outputWithNewLine(("Geben Sie eine Spaltenlaenge fuer das Spielbrett zwischen 7 und 10 ein."));
-				spaltenlaenge = in.nextInt();
-				if (spaltenlaenge < 7 || spaltenlaenge > 10) {
-					OutputCollection.outputWithNewLine(("Ungueltige Eingabe!"));
-					gueltigeEingabe = false;
-				} else {
-					gueltigeEingabe = true;
-				}
-			} while (gueltigeEingabe == false);
+				if (!validMove)
+					System.out.println("Ungueltiger Zug");
 
-			// Auswahl Breite des Spielbretts
-			do {
-				OutputCollection
-						.outputWithNewLine(("Geben Sie eine Zeilenlaenge fuer das Spielbrett zwischen 7 und 10 ein."));
-				zeilenlaenge = in.nextInt();
-				if (zeilenlaenge < 7 || spaltenlaenge > 10) {
-					OutputCollection.outputWithNewLine(("Ungueltige Eingabe!"));
-					gueltigeEingabe = false;
-				} else {
-					gueltigeEingabe = true;
-				}
-			} while (gueltigeEingabe == false);
+				System.out.println("Spieler-2 ist am Zug:");
+				move = sc.next();
 
-			gb = GameBoard.createBoard(spaltenlaenge, zeilenlaenge);
-			gb.printBoard();
-//			if (gspieler == COMPUTER) {
-//				OutputCollection.output("Wenn KI anfangen soll, dann druecke die 3");
-//				if (in.nextInt()==3) {
-//					spieler1="Computer";
-//					spieler2="Spieler";
-//				}
-//				else {
-//					spieler1="Spieler";
-//					spieler2="Computer";
-//				}
-//
-//			} else {
-////			Auswahl Spieler 1/2?
-//			}
-			while (isRunning() == true) {
-				if (spielzug % 2 == 0) {
-					OutputCollection.output("Spieler 1 ist dran: ");
-					spieler=SPIELER_1;
-				} else {
-					OutputCollection.output("Spieler 2 ist dran: ");
-					spieler=SPIELER_1;
-				}
-//				Spielzug.move (spieler,in.next()); //mithilfe spielzug-string spielzug-methode aufrufen
-//				gb.setStein(spieler,1,'d','l');
-				spielzug++;
-			}
-			spielzug = 0;
-//			if (gb.whoWon()==true) {
-//				OutputCollection.outputWithNewLine((spieler1+" hat gewonnen");
-//			}else{
-//				OutputCollection.outputWithNewLine((spieler2+" hat gewonnen");
-//			}
-			OutputCollection.outputWithNewLine(
-					"Wenn Sie das Menu beenden wollen druecken Sie die 0, um ein neues Spiel zu starten druecken Sie eine beliebige andere Zahl");
-			wiederholen = in.nextInt();
-		} while (wiederholen != SPIEL_BEENDEN);
-		in.close();
+			} while (!(validMove = engine.isVaildMove(move)));
+			engine.myMove(move);
+
+
+		}
+		if (!engine.isRunning()) {
+			if (engine.whoWon())
+				System.out.println("Spieler-1 hat gewonnen");
+			else
+				System.out.println("Spieler-2 hat gewonnen");
+		}
+
 	}
 
 }
+
