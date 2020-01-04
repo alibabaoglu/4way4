e905c0b2add87e8405a74bc268ca8b27b284d910
Klopfsalat <58117688+Klopfsalat@users.noreply.github.com>
2020-01-04 13:28:39 +0100

    Update Menu.java

--- 4Way4/src/fourWayFour/Menu.java
+++ 4Way4/src/fourWayFour/Menu.java
@@ -4,114 +4,163 @@ import java.util.*;
 
 public class Menu {
 	final static int COMPUTER = 1;
-	final static int SPIELER = 2;
-	final static int END = 5;
+	final static int PLAYER = 2;
+	final static int END = 0;
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		GameEngine engine;
-		boolean validInput = false, validMove = true;
-		String move = "", erster, zweiter;
-		int row, col, modus;
 
+		boolean validInput = false;
+		String move = "";
+		String first = "";
+		String second = "";
+		int mode;
+		int col;
+		int row;
+		int starter = 0;
+		int difficulty;
+		int counter = 0;
+
+		// ein ganzes Spiel
 		do {
+//			// Auswahl ob Spieler-Spieler oder Spieler-KI
 			do {
-				OutputCollection.outputWithNewLine("Wählen Sie einen Spielmouds:\n \"1\" "
-						+ "fuer ein Spiel gegen die KI\n \"2\" fuer ein Spiel gegen einen anderen Spieler");
-
-				modus = sc.nextInt();
-				if (modus == COMPUTER || modus == SPIELER)
+				OutputCollection.outputWithNewLine(
+						"Waehlen Sie einen Spielmodus:\n   \"1\": Spieler   VS KI\n   \"2\": Spieler-1 VS Spieler-2");
+				mode = sc.nextInt();
+				if (mode == COMPUTER || mode == PLAYER)
 					validInput = true;
 				else
-					OutputCollection.outputWithNewLine("Ungültige Eingabe:" + modus);
+					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + mode);
 			} while (!validInput);
 
+//			// Auswahl der Hoehe des Spielbretts
 			do {
-				OutputCollection
-						.outputWithNewLine("Geben Sie eine Spaltenlaenge fuer das Spielbrett zwischen 7 und 10 ein.");
+				OutputCollection.outputWithNewLine(
+						"Geben Sie die Spaltenlaenge fuer das Spielbrett zwischen \"7\" und \"10\" ein.");
 				col = sc.nextInt();
 				if (col < 7 || col > 10) {
 					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + col);
 					validInput = false;
 				} else
 					validInput = true;
-
 			} while (!validInput);
 
+//			// Auswahl der Breite des Spielbretts
 			do {
-				OutputCollection
-						.outputWithNewLine("Geben Sie eine Zeilenlaenge fuer das Spielbrett zwischen 7 und 10 ein.");
+				OutputCollection.outputWithNewLine(
+						"Geben Sie die Zeilenlaenge fuer das Spielbrett zwischen \"7\" und \"10\" ein.");
 				row = sc.nextInt();
 				if (row < 7 || row > 10) {
-					OutputCollection.outputWithNewLine("Ungueltige Eingabe:" + row);
+					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + row);
 					validInput = false;
 				} else
 					validInput = true;
-
 			} while (!validInput);
 
-			if (modus == COMPUTER) {
-				OutputCollection.outputWithNewLine("Wenn KI anfangen soll, dann druecke die \"3\" ,sonst die \"1\" ");
-				if (sc.nextInt() == 3) {
-					erster = "KI";
-					zweiter = "Spieler";
-				} else {
-					erster = "Spieler";
-					zweiter = "KI";
-				}
-
-			} else {
-				erster = "Spieler-1";
-				zweiter = "Spieler-2";
-			}
-			
-			
-			engine = new GameEngine(row, col, modus, erster);
-			
-			while (engine.isRunning()) {
-
+//			// Wenn Spieler-KI dann wird festgelegt wer anfaengt
+			if (mode == COMPUTER) {
 				do {
-					if (!validMove)
-						OutputCollection.outputWithNewLine("Ungueltiger Zug");
+					OutputCollection.outputWithNewLine("Wer faengt an?:\n   \"1\": KI\n   \"2\": Sie");
+					starter = sc.nextInt();
+					if (starter == COMPUTER) {
+						first = "KI";
+						second = "Spieler";
+						validInput = true;
+					} else if (starter == PLAYER) {
+						first = "Spieler";
+						second = "KI";
+						validInput = true;
+					} else {
+						OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + starter);
+						validInput = false;
+					}
+				} while (!validInput);
+			}
 
-					OutputCollection.outputWithNewLine(erster + " ist am Zug:");
-					if (!erster.equals("KI")) {
+//			// Wenn Spieler-Spieler dann faengt Spieler-1 an
+			else {
+				first = "Spieler-1";
+				second = "Spieler-2";
+			}
+
+//			// Wenn Spieler-KI, Auswahl der Schwierigkeit der KI und erstellen des Spiels
+			if (mode == COMPUTER) {
+				do {
+					OutputCollection.outputWithNewLine(
+							"Waehlen Sie den Schwierigkeitsgrad der KI:\n   \"1\": leichte KI\n   \"2\": mittlere KI\n   \"3\": schwere KI");
+					difficulty = sc.nextInt();
+					if (difficulty >= 1 && difficulty <= 3) {
+						validInput = true;
+					} else {
+						validInput = false;
+						OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + difficulty);
+					}
+				} while (!validInput);
+				engine = new GameEngine(row, col, mode, first);// TODO
+			}
+
+//			// Wenn Spieler-Spieler erstellen des Spiels
+			else {
+				engine = new GameEngine(row, col, mode, first);// TODO
+			}
+
+//			// Der Spielablauf
+			OutputCollection.outputWithNewLine("Spielfeld:");
+			engine.printBoard();
+			while (engine.isRunning()) {
+				counter++;
+
+				// Eingabe des Spielzugs
+				do {
+
+					if (counter % 2 != 0) {
+						OutputCollection.outputWithNewLine(first + " ist am Zug");
+					} else {
+						OutputCollection.outputWithNewLine(second + " ist am Zug");
+					}
+
+					if (mode == COMPUTER) {
+						if ((starter == COMPUTER && counter % 2 != 0) || (starter == PLAYER && counter % 2 == 0)) {
+							move = "";
+							//OutputCollection.outputWithNewLine("Zugeingabe:\n");// TODO
+						} else {
+							OutputCollection.outputWithNewLine("Zugeingabe:");
+							move = sc.next();
+						}
+					}
+					if (mode == PLAYER) {
+						OutputCollection.outputWithNewLine("Zugeingabe:");
 						move = sc.next();
-						validMove = engine.isVaildMove(move);
-					} else
-						break;
+					}
+					validInput = engine.isVaildMove(move);
+					if (!validInput && !(mode == COMPUTER && ((starter == COMPUTER && counter % 2 != 0)
+							|| (starter == PLAYER && counter % 2 == 0)))) {
+						OutputCollection.outputWithNewLine("Ungueltige Eingabe:" + move);
+					}
 
-				} while (!validMove);
+				} while (!validInput && !(mode == COMPUTER
+						&& ((starter == COMPUTER && counter % 2 != 0) || (starter == PLAYER && counter % 2 == 0))));
 
 				engine.myMove(move);
 				engine.printBoard();
-				if (engine.isRunning()) {
-					do {
-						if (!validMove)
-							OutputCollection.outputWithNewLine("Ungueltiger Zug");
-
-						OutputCollection.outputWithNewLine(zweiter + "ist am Zug:");
-						if (!zweiter.equals("KI")) {
-							move = sc.next();
-							validMove = engine.isVaildMove(move);
-						} else
-							break;
-
-					} while (!(validMove = engine.isVaildMove(move)));
-					engine.myMove(move);
-					engine.printBoard();
-				}
 			}
+
+//			// Spielende
+			counter = 0;
 			if (!engine.isRunning()) {
 				if (engine.whoWon())
-					OutputCollection.outputWithNewLine(erster + " hat gewonnen");
+					OutputCollection.outputWithNewLine(first + " hat gewonnen");
 				else
-					OutputCollection.outputWithNewLine(zweiter + " hat gewonnen");
+					OutputCollection.outputWithNewLine(second + " hat gewonnen");
 			}
 
 			OutputCollection.outputWithNewLine(
-					"Wenn Sie aufhören wollen drücken Sie die \"5\", andernfalls drücken Sie eine andere Zahl");
+					"Wenn Sie aufhören wollen drücken Sie die \"0\", andernfalls drücken Sie eine andere Zahl");
 		} while (sc.nextInt() != END);
+		OutputCollection.outputWithNewLine("Das Spiel wurde beendet.");
 		sc.close();
 	}
 }
