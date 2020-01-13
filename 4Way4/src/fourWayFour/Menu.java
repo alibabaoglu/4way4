4d2182e406c31311934564be2e37ec0a77c52756
Klopfsalat <58117688+Klopfsalat@users.noreply.github.com>
2020-01-13 09:53:40 +0100

    Update Menu.java

--- 4Way4/src/fourWayFour/Menu.java
+++ 4Way4/src/fourWayFour/Menu.java
@@ -1,4 +1,4 @@
-package ki2;
+package fourTheWin;
 
 import java.util.*;
 
@@ -6,6 +6,7 @@ public class Menu {
 	final static int COMPUTER = 1;
 	final static int PLAYER = 2;
 	final static int END = 0;
+	final static int RESET = 5;
 
 	public static void main(String[] args) {
 
@@ -16,12 +17,13 @@ public class Menu {
 		String move = "";
 		String first = "";
 		String second = "";
-		int mode;
-		int col;
-		int row;
+		int mode = 0;
+		int col = 0;
+		int row = 0;
 		int starter = 0;
-		int difficulty;
+		int difficulty = 0;
 		int counter = 0;
+		int restart = 0;
 
 		// ein ganzes Spiel
 		do {
@@ -29,20 +31,29 @@ public class Menu {
 			do {
 				OutputCollection.outputWithNewLine(
 						"Waehlen Sie einen Spielmodus:\n   \"1\": Spieler   VS KI\n   \"2\": Spieler-1 VS Spieler-2");
-				mode = sc.nextInt();
+				try {
+					mode = sc.nextInt();
+				} catch (InputMismatchException x) {
+					sc.nextLine();
+				}
 				if (mode == COMPUTER || mode == PLAYER)
 					validInput = true;
 				else
-					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + mode);
+					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + mode + "\n");
 			} while (!validInput);
 
 //			// Auswahl der Hoehe des Spielbretts
 			do {
 				OutputCollection.outputWithNewLine(
 						"Geben Sie die Spaltenlaenge fuer das Spielbrett zwischen \"7\" und \"10\" ein.");
-				col = sc.nextInt();
+				try {
+					col = sc.nextInt();
+				} catch (InputMismatchException x) {
+					sc.nextLine();
+					validInput = false;
+				}
 				if (col < 7 || col > 10) {
-					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + col);
+					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + col + "\n");
 					validInput = false;
 				} else
 					validInput = true;
@@ -52,9 +63,14 @@ public class Menu {
 			do {
 				OutputCollection.outputWithNewLine(
 						"Geben Sie die Zeilenlaenge fuer das Spielbrett zwischen \"7\" und \"10\" ein.");
-				row = sc.nextInt();
+				try {
+					row = sc.nextInt();
+				} catch (InputMismatchException x) {
+					sc.nextLine();
+					validInput = false;
+				}
 				if (row < 7 || row > 10) {
-					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + row);
+					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + row + "\n");
 					validInput = false;
 				} else
 					validInput = true;
@@ -64,7 +80,12 @@ public class Menu {
 			if (mode == COMPUTER) {
 				do {
 					OutputCollection.outputWithNewLine("Wer faengt an?:\n   \"1\": KI\n   \"2\": Sie");
-					starter = sc.nextInt();
+					try {
+						starter = sc.nextInt();
+					} catch (InputMismatchException x) {
+						sc.nextLine();
+						validInput = false;
+					}
 					if (starter == COMPUTER) {
 						first = "KI";
 						second = "Spieler";
@@ -74,7 +95,7 @@ public class Menu {
 						second = "KI";
 						validInput = true;
 					} else {
-						OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + starter);
+						OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + starter + "\n");
 						validInput = false;
 					}
 				} while (!validInput);
@@ -91,20 +112,25 @@ public class Menu {
 				do {
 					OutputCollection.outputWithNewLine(
 							"Waehlen Sie den Schwierigkeitsgrad der KI:\n   \"1\": leichte KI\n   \"2\": mittlere KI\n   \"3\": schwere KI");
-					difficulty = sc.nextInt();
+					try {
+						difficulty = sc.nextInt();
+					} catch (InputMismatchException x) {
+						sc.nextLine();
+						validInput = false;
+					}
 					if (difficulty >= 1 && difficulty <= 3) {
 						validInput = true;
 					} else {
 						validInput = false;
-						OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + difficulty);
+						OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + difficulty + "\n");
 					}
 				} while (!validInput);
-				engine = new GameEngine(row, col, mode, difficulty, first);// TODO
+				engine = new GameEngine(row, col, mode, difficulty, first);
 			}
 
 //			// Wenn Spieler-Spieler erstellen des Spiels
 			else {
-				engine = new GameEngine(row, col, mode);// TODO
+				engine = new GameEngine(row, col, mode);
 			}
 
 //			// Der Spielablauf
@@ -125,7 +151,6 @@ public class Menu {
 					if (mode == COMPUTER) {
 						if ((starter == COMPUTER && counter % 2 != 0) || (starter == PLAYER && counter % 2 == 0)) {
 							move = "";
-							// OutputCollection.outputWithNewLine("Zugeingabe:\n");// TODO
 						} else {
 							OutputCollection.outputWithNewLine("Zugeingabe:");
 							move = sc.next();
@@ -138,7 +163,7 @@ public class Menu {
 					validInput = engine.isVaildMove(move);
 					if (!validInput && !(mode == COMPUTER && ((starter == COMPUTER && counter % 2 != 0)
 							|| (starter == PLAYER && counter % 2 == 0)))) {
-						OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + move);
+						OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + move + "\n");
 					}
 
 				} while (!validInput && !(mode == COMPUTER
@@ -158,8 +183,23 @@ public class Menu {
 			}
 
 			OutputCollection.outputWithNewLine(
-					"Wenn Sie aufhören wollen drücken Sie die \"0\", andernfalls drücken Sie eine andere Zahl");
-		} while (sc.nextInt() != END);
+					"Wenn Sie aufhören wollen drücken Sie die \"0\", andernfalls drücken Sie die \"5\"");
+			do {
+				try {
+					restart = sc.nextInt();
+				} catch (InputMismatchException x) {
+					sc.nextLine();
+					validInput = false;
+				}
+				if (restart == END || restart == RESET) {
+					validInput = true;
+				}
+				else {
+					validInput = false;
+					OutputCollection.outputWithNewLine("Ungueltige Eingabe: " + restart + "\n");
+				}
+			} while (!validInput);
+		} while (restart != END);
 		OutputCollection.outputWithNewLine("Das Spiel wurde beendet.");
 		sc.close();
 	}
